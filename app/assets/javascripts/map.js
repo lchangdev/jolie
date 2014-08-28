c87c1198a18f859822ff59f5663dd0e4e1a98c76
Lawrence Chang <lc.uclakers@gmail.com>
2014-08-28 13:39:15 -0700

    removed zoom optiosn for map, refactored html classes containers to id containers

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -2,4 +2,7 @@ $(document).ready(function() {
 
   var map = L.mapbox.map('map', 'jolieonline.jbjk2k3j', { zoomControl: false }).setView([34.035, -118.253], 15);
 
+  map.scrollWheelZoom.disable();
+
+  new L.Control.Zoom({ position: 'bottomright' }).addTo(map);
 });
